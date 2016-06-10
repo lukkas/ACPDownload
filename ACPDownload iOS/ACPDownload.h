712e6ad01b6311bedcde95112bfb49cf089647b0
Lukasz Kasperek <luk.kasperek@gmail.com>
2016-06-10 16:32:22 +0200

    add missing header

--- ACPDownload iOS/ACPDownload.h
+++ ACPDownload iOS/ACPDownload.h
@@ -16,12 +16,12 @@ FOUNDATION_EXPORT const unsigned char ACPDownload_iOSVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <ACPDownload_iOS/PublicHeader.h>
 
-#import <ACPDownload_iOS/ACPStaticImages.h>
-#import <ACPDownload_iOS/ACPStaticImagesAlternative.h>
-#import <ACPDownload_iOS/ACPIndeterminateGoogleLayer.h>
-#import <ACPDownload_iOS/ACPIndeterminateLayer.h>
-#import <ACPDownload_iOS/ACPProgressLayer.h>
-#import <ACPDownload_iOS/ACPLayerProtocol.h>
-#import <ACPDownload_iOS/ACPStaticImagesProtocol.h>
-#import <ACPDownload_iOS/ACPDownloadConstants.h>
-#import <ACPDownload_iOS/ACPDownloadView.h>
+#import <ACPDownload/ACPStaticImages.h>
+#import <ACPDownload/ACPStaticImagesAlternative.h>
+#import <ACPDownload/ACPIndeterminateGoogleLayer.h>
+#import <ACPDownload/ACPIndeterminateLayer.h>
+#import <ACPDownload/ACPProgressLayer.h>
+#import <ACPDownload/ACPLayerProtocol.h>
+#import <ACPDownload/ACPStaticImagesProtocol.h>
+#import <ACPDownload/ACPDownloadConstants.h>
+#import <ACPDownload/ACPDownloadView.h>
